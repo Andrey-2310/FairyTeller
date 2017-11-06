ecc1594c113cefdaac593aedc31a4afed436d421
Andrey-2310 <andreyredkovskiy@mail.ru>
2017-11-06 08:02:36 +0300

    [FT-4] Mapper layer for Purchase entity was created

---
+++ Sources/Server/fairy-teller/src/main/java/com/rad/fairyteller/service/mapper/PurchaseMapper.java
@@ -0,0 +1,26 @@
+package com.rad.fairyteller.service.mapper;
+
+import com.rad.fairyteller.domain.purchase.Purchase;
+import com.rad.fairyteller.service.dto.PurchaseDto;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PurchaseMapper extends EntityMapper<PurchaseDto, Purchase> {
+    PurchaseMapper INSTANCE = Mappers.getMapper(PurchaseMapper.class);
+
+    @Mappings({
+            @Mapping(target = "userId", source = "purchase.user.id"),
+            @Mapping(target = "workId", source = "purchase.work.id"),
+    })
+    PurchaseDto toDto(Purchase purchase);
+
+    List<PurchaseDto> toDto(List<Purchase> purchaseList);
+
+    @InheritInverseConfiguration
+    Purchase toEntity(PurchaseDto purchaseDto);
+
+    List<Purchase> toEntity(List<PurchaseDto> purchaseDto);
+}
