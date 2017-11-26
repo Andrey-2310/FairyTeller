8161e29510ab3dcf054656fe1c2fa7bc110fea6f
Andrey-2310 <andreyredkovskiy@mail.ru>
2017-11-26 23:08:33 +0300

    [FT-12] intro updated

--- Sources/Client/src/app/modules/startPage/introduction/introduction.component.ts
+++ Sources/Client/src/app/modules/startPage/introduction/introduction.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'introduction',
   templateUrl: './introduction.component.html',
-  styleUrls: ['../../../styles/introduction.component.css'],
+  styleUrls: ['../../../styles/introduction.component.less'],
 })
 export class IntroductionComponent implements OnInit {
 
