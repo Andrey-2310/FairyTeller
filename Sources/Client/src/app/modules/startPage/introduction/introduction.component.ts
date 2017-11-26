6eb8e2644595626a729f07955e084f90421e5b47
Andrey-2310 <andreyredkovskiy@mail.ru>
2017-11-26 20:24:11 +0300

    [FT-12] startPage created

---
+++ Sources/Client/src/app/modules/startPage/introduction/introduction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'introduction',
+  templateUrl: './introduction.component.html',
+  styleUrls: ['../../../styles/introduction.component.css'],
+})
+export class IntroductionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
