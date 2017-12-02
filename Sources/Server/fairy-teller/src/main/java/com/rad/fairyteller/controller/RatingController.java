acf01f2d0714d29e29905d6250dc3e696f5d95ad
Andrey-2310 <andreyredkovskiy@mail.ru>
2017-12-02 14:03:23 +0300

    [FT-14] rating implementation completed

--- Sources/Server/fairy-teller/src/main/java/com/rad/fairyteller/controller/RatingController.java
+++ Sources/Server/fairy-teller/src/main/java/com/rad/fairyteller/controller/RatingController.java
@@ -1,7 +1,8 @@
 package com.rad.fairyteller.controller;
 
-import com.rad.fairyteller.domain.rating.Rating;
 import com.rad.fairyteller.service.RatingService;
+import com.rad.fairyteller.service.dto.ratingDto.RatingDto;
+import com.rad.fairyteller.service.mapper.RatingMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,14 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RatingController {
     private final RatingService ratingService;
+    private final RatingMapper ratingMapper;
 
     @Autowired
-    public RatingController(RatingService ratingService) {
+    public RatingController(RatingService ratingService, RatingMapper ratingMapper) {
         this.ratingService = ratingService;
+        this.ratingMapper = ratingMapper;
     }
 
     @RequestMapping(name = "rating/save", method = RequestMethod.POST)
-    public void saveRating(@RequestParam Rating rating){
-        ratingService.saveRating(rating);
+    public void saveRating(@RequestParam RatingDto ratingDto){
+        ratingService.saveRating(ratingMapper.toEntity(ratingDto));
     }
 }
